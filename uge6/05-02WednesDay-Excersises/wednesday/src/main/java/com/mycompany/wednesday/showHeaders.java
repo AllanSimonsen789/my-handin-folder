bf655909d9c3691bb5c55f3792cf926544d8d3f5
789Michael <allanalli@hotmail.com>
2020-02-06 22:02:21 +0100

    uge6

---
+++ uge6/05-02WednesDay-Excersises/wednesday/src/main/java/com/mycompany/wednesday/showHeaders.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.wednesday;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author allan
+ */
+@WebServlet(name = "showHeaders", urlPatterns = {"/showHeaders"})
+public class showHeaders extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        StringBuilder sb = new StringBuilder();
+                try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet showHeaders</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet showHeaders at " + request.getContextPath() + "</h1>");
+            out.println("<h2> Host: "+ request.getHeader("host")+"</h2>");
+            out.println("<h2> Connection: "+ request.getHeader("connection")+"</h2>");
+            out.println("<h2> cache-control: "+ request.getHeader("cache-control")+"</h2>");
+            out.println("<h2> accept: "+ request.getHeader("accept")+"</h2>");
+            out.println("<h2> user-agent: "+ request.getHeader("user-agent")+"</h2>");
+            out.println("<h2> accept-encoding: "+ request.getHeader("accept-encoding")+"</h2>");
+            out.println("<h2> accept-language: "+ request.getHeader("accept-language")+"</h2>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
